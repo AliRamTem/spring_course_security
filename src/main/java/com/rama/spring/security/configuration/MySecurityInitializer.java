6f55eb3844851f45c0e6b7e0522047538be4d180
AliRamTem <nitro110294@gmail.com>
2021-04-05 13:38:18 +0300

    Initial commit

---
+++ src/main/java/com/rama/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.rama.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
